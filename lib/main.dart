f05fa5de789651454af1965c5f9548c4212d1a5d
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-09-04 14:04:26 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:lucide_icons_flutter/lucide_icons_flutter.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import 'firebase_options.dart';
 import 'core/theme.dart';
