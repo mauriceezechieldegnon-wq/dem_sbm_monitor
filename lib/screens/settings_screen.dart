b376f3a2d360e486e6919b2a2b8229cecae9ec70
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-09-04 14:17:11 +0100

    Fix import path for lucide_icons_flutter package

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:lucide_icons_flutter/lucide_icons_flutter.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import '../core/theme.dart';
 import '../models/subscription.dart';
